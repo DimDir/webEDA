40c2a94f8ae4eefe873c166101fecbbfd43dc0d2
Kerus <gibiskusus@gmail.com>
2020-01-28 00:21:19 +0400

    added a choice of analysis types in sidebar

--- run.py
+++ run.py
@@ -5,59 +5,61 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data_path = st.sidebar.file_uploader('file', type='csv')
+analyse_option = st.sidebar.radio('Choose analysis type', ('Data statistics', 'Features Correlation',
+                                                            'Box-plot', 'Distribution of features'))
+show_all = st.sidebar.checkbox('Show full analysis')
+
 st.title('Web exploratory data analysis')
 
 if data_path is not None:
     df = st.cache(pd.read_csv)(data_path)
-    
-    '''
-    ## Data overview
-    '''
-    
-    st.write(df)
-    '''
-    ## Columns statistics
-    '''
-    # add choice a column type    
-    dtype_option = st.selectbox('Chose column\'s type', ( df.dtypes.value_counts().index))
-    st.write(df.describe(include=[dtype_option]))
-    
-    
-    '''
-    ## Ratio of available data (not NAN's) in percent
-    '''
-    # add values in percent
-    data_ratios = (df.count() / len(df))
-    st.write(data_ratios * 100)  
-    
-    
-    #fig, ax = plt.subplots(figsize=(25, 25))
-    '''
-    ## Heat map of features
-    '''
-    corr = df.select_dtypes(exclude=['object', 'datetime']).corr(method='spearman')
-    sns.heatmap(corr, annot=True,
-                annot_kws={'size': 8})
-    st.pyplot()
+    if analyse_option == 'Data statistics' or show_all:
+        '''
+        ## Data overview
+        '''
+        st.write(df)
+        '''
+        ## Columns statistics
+        '''
+        # add choice a column type
+        dtype_option = st.selectbox('Choose column\'s type', ( df.dtypes.value_counts().index))
+        st.write(df.describe(include=[dtype_option]))
 
-    '''
-    ## Box-plot
-    '''
-    col_for_box_x = st.selectbox('Choose x asix', df.columns)
-    col_for_box_y = st.selectbox('Choose y asix', df.columns)
-    sns.boxplot(x=col_for_box_x, y=col_for_box_y, data=df)
-    st.pyplot()
-    
-    '''
-    ## Distribution of features
-    '''
-    group_cols = st.multiselect('Choose columns for grouping', df.columns)
-    anal_col = st.selectbox('Choose column for analysis', df.columns)
-    if len(group_cols) > 0:
-        df_gr = df.groupby(group_cols)[anal_col]
-    else:
-        df_gr = df[anal_col]
-    df_gr.hist()
-    plt.title('Distribution of {}'.format(anal_col))
-    plt.xlabel(anal_col)
-    st.pyplot()
+        '''
+        ## Ratio of available data (not NAN's) in percent
+        '''
+        # add values in percent
+        data_ratios = (df.count() / len(df))
+        st.write(data_ratios * 100)
+
+    if analyse_option == 'Features Correlation' or show_all:
+        '''
+        ## Heat map of features
+        '''
+        corr = df.select_dtypes(exclude=['object', 'datetime']).corr(method='spearman')
+        sns.heatmap(corr, annot=True,
+                    annot_kws={'size': 8})
+        st.pyplot()
+    if analyse_option=='Box-plot' or show_all:
+        '''
+        ## Box-plot
+        '''
+        col_for_box_x = st.selectbox('Choose x axis', [col for col in df.columns if df[col].dtype not in ['object']])
+        col_for_box_y = st.selectbox('Choose y axis', [col for col in df.columns if df[col].dtype not in ['object']])
+        sns.boxplot(x=col_for_box_x, y=col_for_box_y, data=df)
+        st.pyplot()
+    if analyse_option == 'Distribution of features' or show_all:
+        '''
+        ## Distribution of features
+        '''
+        group_cols = st.multiselect('Choose columns for grouping', df.columns)
+        anal_col = st.selectbox('Choose column for analysis', [col for col in df.columns if df[col].dtype not in ['object']])
+        if len(group_cols) > 0:
+            df_gr = df.groupby(group_cols)[anal_col]
+        else:
+            df_gr = df[anal_col]
+        df_gr.hist()
+        plt.title('Distribution of {}'.format(anal_col))
+        plt.xlabel(anal_col)
+        st.pyplot()
+
