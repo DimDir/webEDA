44cb81cf3e7ac1aa0e09a3860df7997c865dbd06
DimDir <borkin.dmitriy@gmail.com>
2020-01-28 13:27:23 +0100

    add first ML model

--- run.py
+++ run.py
@@ -4,12 +4,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data_path = st.sidebar.file_uploader('file', type='csv')
-analyse_option = st.sidebar.radio('Choose analysis type', ('Data statistics', 'Features Correlation',
-                                                            'Box-plot', 'Distribution of features'))
-show_all = st.sidebar.checkbox('Show full analysis')
+########### List of functions
 
+
+# Use for splitting data for training models
+
+def split_df(df, beta=.7):
+    """Split DataFrame on tran and test subsets."""
+    split = int(df.shape[0] * beta) 
+    X = np.array(df.drop(x, axis=1))
+    y = df[x]
+    X_train, X_valid, y_train, y_valid = X[:split], X[split:], y[:split], y[split:]
+    return X_train, X_valid, y_train, y_valid
+
+
+# Control datasets on object's type
+
+def type_control(df):
+    """Delete types "object" from dataset."""
+    df = df.select_dtypes(exclude='object')
+    return df
+
+###########
+
+data_path = st.sidebar.file_uploader('file', type='csv')
+analyse_option = st.sidebar.radio('Choose analysis type', ('Data statistics', 'Features Correlation', 'Box-plot', 'Distribution of features'))
+
+show_all = st.sidebar.checkbox('Show full analysis')
 st.title('Web exploratory data analysis')
+
 
 if data_path is not None:
     df = st.cache(pd.read_csv)(data_path)
@@ -18,6 +41,7 @@ if data_path is not None:
         ## Data overview
         '''
         st.write(df)
+       
         '''
         ## Columns statistics
         '''
@@ -40,6 +64,7 @@ if data_path is not None:
         sns.heatmap(corr, annot=True,
                     annot_kws={'size': 8})
         st.pyplot()
+        
     if analyse_option=='Box-plot' or show_all:
         '''
         ## Box-plot
@@ -48,6 +73,7 @@ if data_path is not None:
         col_for_box_y = st.selectbox('Choose y axis', [col for col in df.columns if df[col].dtype not in ['object']])
         sns.boxplot(x=col_for_box_x, y=col_for_box_y, data=df)
         st.pyplot()
+        
     if analyse_option == 'Distribution of features' or show_all:
         '''
         ## Distribution of features
@@ -63,3 +89,24 @@ if data_path is not None:
         plt.xlabel(anal_col)
         st.pyplot()
 
+       
+        
+if st.sidebar.checkbox('model builder'):
+    
+    """
+    ## Constructor
+    """
+    
+    st.write("model builder")
+    x = st.radio('Choose the target:', (df.columns))
+    st.write('Now target is: ', x)
+    
+    if st.button('calculate'):
+        from sklearn.neighbors import KNeighborsClassifier
+        model = KNeighborsClassifier()
+        new_df = type_control(df)
+        X_train, X_valid, y_train, y_valid = split_df(new_df)
+        model.fit(X_train, y_train)
+       
+        prediction = model.score(X_valid, y_valid)
+        "Model accuracy: ", prediction
